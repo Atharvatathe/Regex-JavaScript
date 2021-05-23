1de67a9ae817772c2014ae550230991c66e055ab
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-23 17:13:43 +0530

    Added:Test pincode has Space in between pincode 3 digit

--- Pincode.js
+++ Pincode.js
@@ -1,4 +1,4 @@
-//UC1
+//UC2
 class Pincode{
 
     get pin(){
@@ -6,12 +6,13 @@ class Pincode{
     }
 
     set pincode(pincode){
-        let pinRegex = RegExp('^[0-9]{6}$');
+        let pinRegex = RegExp('^[0-9]{3}[\\s][0-9]{3}$');
         if(pinRegex.test(pincode))
-            this._pincode = pincode;
-        else throw 'Wrong Pin Code';  
-    }
-    toString(){
+            this._pincode = pincode; 
+        else throw 'Wrong Pin Code';
+      }
+    
+      toString(){
         return "Pincode: " +this._pincode;
     }
 }
@@ -19,7 +20,7 @@ class Pincode{
 
 let pincodeRegex = new Pincode();
 try{
-pincodeRegex.pincode = "400088";
+pincodeRegex.pincode = "400 088";
 console.log(pincodeRegex.toString());
 }catch (e){
     console.error(e);
