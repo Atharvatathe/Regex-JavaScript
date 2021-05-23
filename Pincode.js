3d800fb33c86a5ab105a68a5cf52860405a267db
Atharvatathe <atharva.tathe55@gmail.com>
2021-05-23 16:51:30 +0530

    Added:Test pincode is 6 digit code if wrong throw error

--- Pincode.js
+++ Pincode.js
@@ -1,1 +1,26 @@
-console.log("Regex program using javasript");
+//UC1
+class Pincode{
+
+    get pin(){
+        return this._pincode;
+    }
+
+    set pincode(pincode){
+        let pinRegex = RegExp('^[0-9]{6}$');
+        if(pinRegex.test(pincode))
+            this._pincode = pincode;
+        else throw 'Wrong Pin Code';  
+    }
+    toString(){
+        return "Pincode: " +this._pincode;
+    }
+}
+
+
+let pincodeRegex = new Pincode();
+try{
+pincodeRegex.pincode = "400088";
+console.log(pincodeRegex.toString());
+}catch (e){
+    console.error(e);
+}
